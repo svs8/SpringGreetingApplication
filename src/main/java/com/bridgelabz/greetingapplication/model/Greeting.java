a012e5e2e73b2c1ee3dd3f79b82a822a5f7ff3e4
svs8 <surakshithvittalshetty@gmail.com>
2022-07-27 13:22:48 +0530

    Using GreetingController to return JSON for HTTP Method.

---
+++ src/main/java/com/bridgelabz/greetingapplication/model/Greeting.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.greetingapplication.model;
+
+import lombok.Data;
+
+@Data
+public class Greeting {
+        private long id;
+        private String content;
+
+        public Greeting(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+}
